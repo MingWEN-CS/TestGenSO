109b1c983334fab346ff3dbae041d003f17464e0
justinwm <justinwm@163.com>
2016-06-14 11:32:47 +0800

    transform to multiple threads

--- src/tasks/GenerateTestCases.java
+++ src/tasks/GenerateTestCases.java
@@ -1,6 +1,8 @@
 package tasks;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import config.ParsingArguments;
 import utils.TestCommandHelp;
@@ -24,18 +26,54 @@ public class GenerateTestCases {
 		String targetLibraryAndDependancy = prefix + File.separator + "lib" + File.separator + "*";
 		int timeLimit = 180;
 		String workingPath = ".";
+		
+		String testCaseDir = prefix + File.separator + "randoop-tests";
+		file = new File(testCaseDir);
+		if (!file.exists())
+			file.mkdir();
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 		for (int seed = 0; seed < 10; seed++) {
-			String outputDir = prefix + File.separator + "randoop-tests-" + timeLimit + "-" + seed;
+			String outputDir = testCaseDir + File.separator + "randoop-tests-" + timeLimit + "-" + seed;
 			file = new File(outputDir);
 			if (!file.exists())
 				file.mkdir();
-			System.out.println("== Generating Randoop test cases with seed:" + seed + " ==");
-			TestCommandHelp.generateRandoopTestCases(targetLibraryAndDependancy, classlist, seed, timeLimit, outputDir, workingPath);
+			Runnable work = new runRandoopTestCasesPerSeed(targetLibraryAndDependancy, classlist, seed, timeLimit, outputDir, workingPath);
+			executor.execute(work);
 		}
+		executor.shutdown();
 	}
+	
 	
 	public static void main(String[] args) {
 		ParsingArguments.parsingArguments(args);
 		generateRandoopTestCases();
 	}
+}
+
+class runRandoopTestCasesPerSeed implements Runnable {
+	
+	String targetLibraryAndDependancy;
+	String classlist;
+	int seed;
+	int timeLimit;
+	String outputDir;
+	String workingPath;
+	
+	public runRandoopTestCasesPerSeed(String targetLibraryAndDependancy, String classlist, int seed, int timeLimit, String outputDir, String workingPath) {
+		this.targetLibraryAndDependancy = targetLibraryAndDependancy;
+		this.classlist = classlist;
+		this.seed = seed;
+		this.timeLimit = timeLimit;
+		this.outputDir = outputDir;
+		this.workingPath = workingPath;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		long threadId = Thread.currentThread().getId();
+		System.out.println("== Thread: " + threadId + "Generating Randoop test cases with seed:" + seed + " ==");
+		TestCommandHelp.generateRandoopTestCases(targetLibraryAndDependancy, classlist, seed, timeLimit, outputDir, workingPath);
+	}
+	
 }
