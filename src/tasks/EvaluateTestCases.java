2220fec8b68a32f9176e7a62286450e6e5fe3abe
justinwm <justinwm@163.com>
2016-06-16 21:09:30 +0800

    minor issue

--- src/tasks/EvaluateTestCases.java
+++ src/tasks/EvaluateTestCases.java
@@ -195,6 +195,7 @@ public class EvaluateTestCases {
 				
 				String classname = relativePath.substring(relativePath.indexOf(config.Config.libToPackage.get(targetLibrary).replace(".", "/")));
 				classname = classname.replace("/", ".");
+				classname = classname.substring(0, classname.length() - 5);
 				System.out.println("Running JUnit Test Cases on " + classname);
 				Pair<String,String> results = TestCommandHelp.runJUnitTestCases(dependancies, testCaseDir, 
 						classname, workingPath);
