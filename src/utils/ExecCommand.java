3fa302495882eea62f122571520141265596e0f9
justinwm <justinwm@163.com>
2016-06-17 14:30:57 +0800

    normal update

--- src/utils/ExecCommand.java
+++ src/utils/ExecCommand.java
@@ -33,30 +33,37 @@ public class ExecCommand {
 	}
 	
 	public Pair<String,String> execOneThread(String[] commands, String workingpath) {
-		final StringBuffer result = new StringBuffer("");
-		final StringBuffer errors = new StringBuffer("");
+		ReadStream s1,s2;
+		Pair<String,String> result = null;
 		try {
 			File dir = new File(workingpath);
 			Process process = Runtime.getRuntime().exec(commands, null, dir);
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-			String line = null;
-			while ((line = stdInput.readLine()) != null) {
-				System.out.println(line);
-				result.append(line + "\n");
-			}
 			
-			while ((line = stdError.readLine()) != null) {
-				System.err.println(line);
-				errors.append(line + "\n");
-			}
+			s1 = new ReadStream("stdin",process.getInputStream());
+			s2 = new ReadStream("stderr", process.getErrorStream());
+			s1.start();
+			s2.start();
+			process.waitFor();
 			
-			stdInput.close();
-			stdError.close();
+//			String line = null;
+//			while ((line = stdInput.readLine()) != null) {
+//				System.out.println(line);
+//				result.append(line + "\n");
+//			}
+//			
+//			while ((line = stdError.readLine()) != null) {
+//				System.err.println(line);
+//				errors.append(line + "\n");
+//			}
+//			
+//			stdInput.close();
+//			stdError.close();
+			result = new Pair<String,String>(s1.output,s2.output);
 		} catch (Exception e) {
 			System.err.println("Error:" + e.getClass());
-			return new Pair<String,String>(result.toString(), errors.toString());
+			return result;
 		}
-		return new Pair<String,String>(result.toString(), errors.toString());
+		return result;
 	}
 }
+
